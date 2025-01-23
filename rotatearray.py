d2dde1748a8ad5e711286e7009cbd6f37234399d
anga kishor <147230833+angakishor@users.noreply.github.com>
2025-01-23 13:37:50 +0530

    Update rotatearray.py
    rotate array with k times

--- rotatearray.py
+++ rotatearray.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        while(k!=0):
+            lst=nums.pop()
+            nums.insert(0,lst)
+            k-=1
+        return nums
