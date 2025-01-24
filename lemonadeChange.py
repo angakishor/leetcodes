d418c564df5fdf1dabfca2225e13cd9372189bb0
anga kishor <147230833+angakishor@users.noreply.github.com>
2025-01-24 08:54:04 +0530

    Update lemonadeChange.py
    solution of lemonade change in python

--- lemonadeChange.py
+++ lemonadeChange.py
@@ -1,1 +1,19 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        f=0
+        t=0
+        for i in bills:
+            if i==5:
+                f+=1
+            elif i==10:
+                f-=1
+                t+=1
+            elif t>0:
+                t-=1
+                f-=1
+            else:
+                f -=3
 
+            if f <0 :
+                return False
+        return True
